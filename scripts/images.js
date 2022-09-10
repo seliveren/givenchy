7baf57270a2960213c12ad290e60e8c5e4c8df14
Yulia Selivanova <selivanyulia@mail.ru>
2022-09-10 18:12:48 +0300

    fix: changes to code format

--- scripts/images.js
+++ scripts/images.js
@@ -3,60 +3,50 @@ const images = [
     src: './images/grid_1.png',
     alt: 'Model with acid-green background behind them.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_2.png',
     alt: 'Model with a bag in their hands.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_3.png',
     alt: 'Model with glasses on, close shot.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_4.png',
     alt: 'Model with red dreads sitting on a white cube.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_5.png',
     alt: 'Model dressed in pink with orange background behind them.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_6.png',
     alt: 'Model dressed in black with red background behind them.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_7.png',
     alt: 'Model with long dreads dressed in black top and red bottom.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_8.png',
     alt: 'Model with a bag standing in front of turquoise background.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_9.png',
     alt: 'Topless model with a backpack in their hands.',
     class: 'looks__image'
-
   },
   {
     src: './images/grid_10.png',
     alt: 'Model with a beige bag in front of turquoise background.',
     class: 'looks__image'
-
   }
 ]
